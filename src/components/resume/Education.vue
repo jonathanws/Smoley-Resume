9ff12cee8826ad46df4bba10d8df90193ee71fee
Jon Smoley <jonathanws18@gmail.com>
2018-08-08 01:14:59 -0400

    Added more information about college education

--- src/components/resume/Education.vue
+++ src/components/resume/Education.vue
@@ -1,35 +1,107 @@
 <template>
-	<div class="education box-shadow">
-		<div class="school">
-			<img src="../../assets/education-towson.png">
-			<div class="description">
-				<h4>Towson University</h4>
+	<div class="education">
+
+		<div v-for="(school, index) in schools" v-bind:key="school.name" :class="index == 0 ? 'active' : ''" class="school box-shadow">
+			<div class="school-icon">
+				<img :src="school.asset">
+			</div>
+			<div class="details">
+				<div class="name-and-dates" style="display: flex; align-items: center;">
+					<h4 v-if="index == 0" class="school-name">{{school.name}}</h4>
+					<h5 v-else class="school-name">{{school.name}}</h5>
+
+					<i v-if="school.dates">{{school.dates}}</i>
+				</div>
+
+				<p v-if="school.degree">Obtained<span class="degree" style="padding-left: 3px;">{{school.degree}}</span></p>
+
+				<p class="school-description">{{school.description}}</p>
 			</div>
 		</div>
-		<img src="../../assets/education-davisandelkins.png">
-		lol
+
 	</div>
 </template>
 
 <script>
 export default {
-	
+	data: function() {
+		return {
+			schools: [
+				{
+					asset: require('../../assets/education-towson.png'),
+					name: 'Towson University',
+					degree: 'Bachelor of Science in Computer Science',
+					dates: '2010-2014',
+					description: 'Towson University Marching Band -- Drumline\nWorked for university\'s Mobile Development team'
+					},
+				{
+					asset: require('../../assets/education-davisandelkins.png'),
+					name: 'Davis & Elkins College',
+					dates: '2009-2010',
+					description: 'Davis and Elkins Alpine Ski Racing Team, Student Assembly Representative, IT desk assistant'
+					},
+				{ asset: require('../../assets/education-frederickcommunity.jpg'), name: 'Frederick Community College' }
+			]
+		}
+	}
 }
 </script>
 
 <style lang="scss" scoped>
-	.education {
-		.school {
-			display: flex;
-			>img {
-				flex-basis: 33%;
-				display: block;
-				max-width: 100%;
-				height: auto;
+@import 'styles/constants';
+
+$img-size: 100px;
+$padding: 8px;
+
+.education {
+	.school {
+		display: flex;
+		margin-bottom: 16px;
+		&.active {
+			.school-icon {
+				flex-basis: ($img-size - ($padding * 2)) * 2;
+				height: ($img-size - ($padding * 2)) * 2;
+				padding: $padding * 2;
+				img {
+					max-height: ($img-size - ($padding * 2)) * 2;
+					max-width: ($img-size - ($padding * 2)) * 2;
+				}
 			}
-			.description {
-				flex-basis: 64%;
+		}
+		.school-icon {
+			flex-basis: $img-size - ($padding * 2);
+			display: flex;
+			align-items: center;
+			height: $img-size - ($padding * 2);
+			padding: $padding;
+			background-color: white;
+			img {
+				max-height: $img-size - ($padding * 2);
+				max-width: $img-size - ($padding * 2);
+			}
+		}
+		.details {
+			flex-grow: 1;
+			margin-left: 10px;
+			margin-right: 10px;
+			.name-and-dates {
+				display: flex;
+				align-items: center;
+				.school-name {
+					flex-grow: 1;
+				}
+				i {
+					font-size: 12px;
+				}
+			}
+			.school-name,
+			.degree {
+				color: $accent-color;
+			}
+			p {
+				margin-top: 0;
 			}
 		}
 	}
+}
 </style>
